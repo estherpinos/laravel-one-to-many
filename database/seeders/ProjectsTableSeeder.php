2f5227297e0aa901bb58f5c4113d268d6c9f1e02
estherpinos <estherpinos1@gmail.com>
2023-12-04 15:07:47 +0100

    Bonus [crud type and migration (era già fatto)]

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
+use App\Models\Type;
+
 use Faker\Generator as Faker;
 
 
@@ -20,6 +22,7 @@ class ProjectsTableSeeder extends Seeder
     {
         for($i = 0; $i < 10; $i++) {
             $new_project = new Project();
+            $new_project->type_id=Type::inRandomOrder()->first()->id;
             $new_project->title = $faker->sentence(3);
             $new_project->description = $faker->paragraph();
             $new_project->save();
